37c761a670234c4d36ef2b731290c386886103c2
Chris McHarg <chris@raspberrypi3.(none)>
2014-10-28 23:33:36 +0000

    Added hello.py file to repository

---
+++ hello.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+name = ''
+
+while name != 'quit' :
+	name = raw_input('What is your name? ')
+
+	if name == 'Chris':
+		print "Chris, you're incredible."
+	else :
+		print 'Hello,', name
+
